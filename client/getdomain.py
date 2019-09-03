3207524345504b55609447af1b4b270dca94541c
Karl Koscher <supersat@cs.washington.edu>
2019-09-03 16:54:57 -0700

    Add support for ACME_ACCT_KEY to getdomain.py

--- client/getdomain.py
+++ client/getdomain.py
@@ -4,6 +4,7 @@ import argparse
 import base36
 import base64
 import jwcrypto.jwk
+import os
 
 def get_domain(key, domain):
     priv_key = jwcrypto.jwk.JWK.from_json(open(key, 'rt').read())
@@ -11,10 +12,20 @@ def get_domain(key, domain):
     thumbprint = base36.dumps(int.from_bytes(thumbprint_bytes, byteorder='big'))
     return '*.{}.{}'.format(thumbprint, domain)
 
+# https://stackoverflow.com/a/45392259
+def environ_or_required(key):
+    if os.environ.get(key):
+        return {'default': os.environ.get(key)}
+    else:
+        return {'required': True}
+
 def main():
     arg_parser = argparse.ArgumentParser(description='TLSMy.net domain generator')
-    arg_parser.add_argument('-k', '--key', help='certbot account private key', required=True)
-    arg_parser.add_argument('-d', '--domain', help='Host domain. Defaults to tlsmy.net', default='tlsmy.net')
+    arg_parser.add_argument('-k', '--key', help='certbot account private key. ' \
+        'May also be passed in via the ACME_ACCT_KEY environment variable.', \
+        **environ_or_required('ACME_ACCT_KEY'))
+    arg_parser.add_argument('-d', '--domain', help='Host domain. Defaults to tlsmy.net', \
+        default='tlsmy.net')
     args = arg_parser.parse_args()
     print(get_domain(args.key, args.domain))
 
