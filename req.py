6290adaf782a331da254182ad835d28536358022
Karl Koscher <github@degdeg.com>
2019-09-02 18:25:30 -0700

    Add command-line arguments to req.py

--- req.py
+++ req.py
@@ -4,6 +4,7 @@ import argparse
 import json
 import jwcrypto.jwk
 import jwcrypto.jws
+import os
 import urllib.request
 
 def sign(payload, priv_key_json):
@@ -16,15 +17,30 @@ def sign(payload, priv_key_json):
     }))
     return jws.serialize()
 
-def test(token):
-    priv_key_file = open('private_key.json', 'rt')
+def test(key, validation_string, url):
+    priv_key_file = open(key, 'rt')
     priv_key_json = priv_key_file.read()
-    req = urllib.request.Request('https://tlsmy.net/challenge',
+    req = urllib.request.Request(url,
         data=sign(json.dumps({"type": "dns-01", "token": token}), priv_key_json).encode('utf-8'),
         headers={"Content-Type": "application/jose+json"})
     urllib.request.urlopen(req)
 
-def main():
-    test('this is a test token')
+# https://stackoverflow.com/a/45392259
+def environ_or_required(key):
+    if os.environ.get(key):
+        return {'default': os.environ.get(key)}
+    else:
+        return {'required': True}
 
-main()
+def main():
+    arg_parser = argparse.ArgumentParser(description='TLSMy.net challenge requester')
+    arg_parser.add_argument('-k', '--key', help='certbot account private key', required=True)
+    arg_parser.add_argument('-V', '--validation-string', help='ACME DNS-01 validation string.' \
+        'May also be passed in via the CERTBOT_VALIDATION environment variable.', \
+            **environ_or_required('CERTBOT_VALIDATION'))
+    arg_parser.add_argument('-u', '--url', help='HTTP(S) API endpoint URL.', default='https://tlsmy.net/challenge')
+    args = arg_parser.parse_args()
+    send_request(**vars(args))
+
+if __name__ == '__main__':
+    main()
