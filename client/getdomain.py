6d8845b497ba3bc14434021d5b9f8fa0d2cfbd5a
Max Vohra <max@seattlenetworks.com>
2020-04-12 12:55:45 -0700

    Adding --cname feature to getdomain.py

--- client/getdomain.py
+++ client/getdomain.py
@@ -6,11 +6,14 @@ import base64
 import jwcrypto.jwk
 import os
 
-def get_domain(key, domain):
+def get_domain(key, domain, cname):
     priv_key = jwcrypto.jwk.JWK.from_json(open(key, 'rt').read())
     thumbprint_bytes = base64.urlsafe_b64decode(priv_key.thumbprint() + '==')
     thumbprint = base36.dumps(int.from_bytes(thumbprint_bytes, byteorder='big'))
-    return '*.{}.{}'.format(thumbprint, domain)
+    if cname is None:
+        return '*.{}.{}'.format(thumbprint, domain)
+    else:
+        return '_acme_challenge.{} IN CNAME _acme-challenge.{}.{}'.format(cname, thumbprint, domain)
 
 # https://stackoverflow.com/a/45392259
 def environ_or_required(key):
@@ -26,8 +29,10 @@ def main():
         **environ_or_required('ACME_ACCT_KEY'))
     arg_parser.add_argument('-d', '--domain', help='Host domain. Defaults to tlsmy.net', \
         default='tlsmy.net')
+    arg_parser.add_argument('-c', '--cname', help='Print the required cname entry for authorizing a subdomain through TLSMynet',
+        default=None)
     args = arg_parser.parse_args()
-    print(get_domain(args.key, args.domain))
+    print(get_domain(args.key, args.domain, args.cname))
 
 if __name__ == '__main__':
     main()
